0859e01e3d1ef4a0f29b8bc3453e053946959a02
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-11-30 05:57:42 +0200

    handle errors
    The main goal of this application is defining different CI steps to perform different tasks. To handle errors for each step in a particular way you’d have to create different error values, which isn’t ideal. Instead, for handling these errors you can define your own custom error types.

---
+++ errors.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//stepErr represent a class of errors associated with CI steps,
+//step to record the step name in an error; a message msg that
+//describe the condition; and a cause to store the underlying error
+//that caused this step error
+type stepErr struct {
+	step  string
+	msg   string
+	cause error
+}
+
+func (s *stepErr) Error() string {
+	return fmt.Sprintf("Step: %q: %s: Cause: %v", s.step, s.msg, s.cause)
+}
+
+func (s *stepErr) Is(target error) bool {
+	t, ok := target.(*stepErr)
+	if !ok {
+		return false
+	}
+
+	return t.step == s.step
+}
+
+//Unwrap try to unwrap the error to see
+//if an underlying error matches the target,
+//it returns the error stored in the cause field
+func (s *stepErr) Unwrap() error {
+	return s.cause
+}
